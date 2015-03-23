c9c7c35a9a1b9b4a90ce8ccd8bce9877f44e5445
Andrew Seward <awswrd@gmail.com>
2015-03-22 23:21:31 -0700

    Some testing on Notice schema

---
+++ Bugsnag.NET.Tests/Request/NoticeBuilder.cs
@@ -0,0 +1,76 @@
+﻿using Bugsnag.NET.Request;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.NET.Tests.Request
+{
+    class NoticeBuilder
+    {
+        static IUser _user = new User(42)
+        {
+            Name = "Hamburger Helper",
+            Email = "hamburger@helper.io"
+        };
+
+        static IApp _app = new App
+        {
+            Version = "3.1.4",
+            ReleaseStage = "development"
+        };
+
+        static string _apiKey = "wh47AB0gu5Ap1Key7h1sI5";
+
+        static object _metaData = new
+        {
+            SomeData = new
+            {
+                Id = 12345,
+                CreatedAt = DateTime.Now
+            }
+        };
+
+        public static INotice GetNotice()
+        {
+            try
+            {
+                _OuterMethod();
+                throw new InvalidOperationException("Shouldn't get here...");
+            }
+            catch (Exception ex)
+            {
+                Bugsnag.App = _app;
+                var client = Bugsnag.Error;
+                var evt = client.GetEvent(ex, _user, _metaData);
+                return new Notice(_apiKey, new Notifier(), evt);
+            }
+        }
+
+        static void _OuterMethod()
+        {
+            try
+            {
+                _InnerMethod();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("This is the second wrapping exception", ex);
+            }
+        }
+
+        static void _InnerMethod()
+        {
+            try
+            {
+                var zero = 0;
+                var garbage = 1 / zero;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("This is the first wrapping exception", ex);
+            }
+        }
+    }
+}
---
+++ Bugsnag.NET.Tests/Request/NoticeTests.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bugsnag.NET.Tests.Request
+{
+    class NoticeTests
+    {
+        [Test]
+        public void HasCorrectSchema()
+        {
+            var schema = _GetSchema();
+            var notice = NoticeBuilder.GetNotice();
+            var json = JsonConvert.SerializeObject(notice);
+            var jObject = JObject.Parse(json);
+
+            Assert.IsTrue(jObject.IsValid(schema));
+        }
+
+        JsonSchema _GetSchema()
+        {
+            var json = File.ReadAllText("Data/sampleRequest.json");
+            return JsonSchema.Parse(json);
+        }
+    }
+}
