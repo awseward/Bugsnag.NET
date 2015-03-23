a3dff4eec31d13a51e70c7394fbe9eb95e1ca528
Andrew Seward <awswrd@gmail.com>
2015-03-23 02:24:59 -0700

    Slightly less hardcoded build task

--- rakefile.rb
+++ rakefile.rb
@@ -3,5 +3,6 @@ Dir.glob('tasks/*.rake').each { |r| import r }
 desc "Builds the project"
 task :build => ['nuget:clean', 'nuget:restore'] do
   # FIXME: Un-hardcode this
-  sh "MSBuild.exe Bugsnag.NET.sln /p:Configuration=Release /target:'Clean;Rebuild'"
+  sln = Dir.glob('*.sln').first
+  sh "MSBuild.exe #{sln} /p:Configuration=Release /target:'Clean;Rebuild'"
 end
