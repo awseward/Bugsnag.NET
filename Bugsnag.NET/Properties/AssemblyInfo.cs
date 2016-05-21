b6fbda57f5bc94ea96bdd352d0f05854f77214d0
Andrew William Seward <awswrd@gmail.com>
2016-05-20 19:28:32 -0700

    Move versions to SharedAssemblyInfo file
    Also link that file in each project.

--- Bugsnag.NET/Properties/AssemblyInfo.cs
+++ Bugsnag.NET/Properties/AssemblyInfo.cs
@@ -17,17 +17,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("72121f80-f8ac-4be5-b914-4f65faffc1cc")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.4.3.0")]
-[assembly: AssemblyFileVersion("0.4.3.0")]
-[assembly: AssemblyInformationalVersion("0.4.3")]
