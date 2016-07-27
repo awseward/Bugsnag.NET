61bf25333a37e4b8f339a65a0787a0066dc5485a
Mathew Glodack <glodack@gmail.com>
2016-07-27 13:50:28 -0700

    Update Bugsnag.PCL UnWrap extension method

--- Bugsnag.PCL/Extensions/Extensions.cs
+++ Bugsnag.PCL/Extensions/Extensions.cs
@@ -19,7 +19,7 @@ namespace Bugsnag.PCL.Extensions
                 return new Exception[] { ex };
             }
 
-            return ex.InnerException.Unwrap().Concat(new Exception[] { ex });
+            return new Exception[] { ex }.Concat(ex.InnerException.Unwrap());
         }
 
         public static IEnumerable<string> ToLines(this Exception ex)
