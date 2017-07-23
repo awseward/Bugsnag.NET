8896312c58ff01c0d107c82b4210d3c3ea04e918
Drew Seward <awswrd@gmail.com>
2017-07-23 15:57:09 -0700

    Simplify build script quite a bit

--- ._fake/loader.fsx
+++
@@ -1,3 +0,0 @@
-#r    @"packages/FAKE/tools/FakeLib.dll"
-#r    @"packages/FSharp.FakeTargets/tools/FSharp.FakeTargets.dll"
-#load @"config.fsx"
--- build.fsx
+++ build.fsx
@@ -1,33 +1,27 @@
-#load @"._fake/loader.fsx"
+#r @"._fake/packages/FAKE/tools/FakeLib.dll"
+#r @"._fake/packages/FSharp.FakeTargets/tools/FSharp.FakeTargets.dll"
 
 open Fake
 open NuGetHelper
 open RestorePackageHelper
-open Bugsnag.NET.Fake.Config
 
 datNET.Targets.initialize (fun p ->
   { p with
-      AccessKey = Nuget.ApiKey
-      AssemblyInfoFilePaths = Build.AssemblyInfoFilePaths
-      Authors = Release.Authors
-      Description = Release.Description
-      OutputPath = Release.OutputPath
-      Project = Release.Project
-      ProjectFilePath = Some "Bugsnag.NET/Bugsnag.NET.csproj"
-      Publish = true
-      TestAssemblies = Build.TestAssemblies
-      WorkingDir = Release.WorkingDir
+      AccessKey             = environVar "BUGSNAG_NET_NUGET_API_KEY"
+      AssemblyInfoFilePaths = ["SharedAssemblyInfo.cs"]
+      Authors               = ["Andrew Seward"; "Mathew Glodack"]
+      Description           = "A Bugsnag notifier client for .NET projects"
+      OutputPath            = "bin"
+      Project               = "Bugsnag.NET"
+      ProjectFilePath       = Some "Bugsnag.NET/Bugsnag.NET.csproj"
+      Publish               = true
+      TestAssemblies        = !! "Bugsnag.NET.Tests/**/bin/**/*.Tests.dll"
+      WorkingDir            = "bin" // NOTE: This seems a bit off for the label's name of "WorkingDir"
   }
 )
 
 Target "RestorePackages" (fun _ ->
-  Source.SolutionFile
-  |> Seq.head
-  |> RestoreMSSolutionPackages (fun parameters ->
-      { parameters with
-          Sources = [ "https://nuget.org/api/v2"; ]
-          OutputPath = "packages"
-          Retries = 4 })
+  "Bugsnag.NET.sln" |> RestoreMSSolutionPackages id
 )
 
 "MSBuild" <== [ "Clean"; "RestorePackages" ]
