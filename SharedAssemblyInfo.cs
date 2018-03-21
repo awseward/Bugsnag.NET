cf52c1bf6883e506cf5d90f2a1cbed0c5b099843
Drew Seward <awswrd@gmail.com>
2018-03-20 19:35:43 -0700

    version: Pre-increment minor -> 0.11.0-b

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -2,6 +2,6 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
-[assembly: AssemblyInformationalVersion("0.10.0")]
+[assembly: AssemblyVersion("0.11.0.0")]
+[assembly: AssemblyFileVersion("0.11.0.0")]
+[assembly: AssemblyInformationalVersion("0.11.0-b")]
